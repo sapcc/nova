9869a01540d31b6efc16ac3b7dcb67f5d2275ce8
Gary Kotton <gkotton@vmware.com>
2016-04-11 01:59:06 -0700

    VMware: add in context for log messages
    In vmops.py there were a few cases where log messages did not have
    the instance ID. This makes debugging very difficult.
    
    TrivialFix
    
    Change-Id: I95353a9de2780bf1b8702ee3e10079bce52427ef

--- nova/virt/vmwareapi/vmops.py
+++ nova/virt/vmwareapi/vmops.py
@@ -424,7 +424,8 @@ class VMwareVMOps(object):
             host, cookies = self._get_esx_host_and_cookies(vi.datastore,
                 dc_path, image_ds_loc.rel_path)
         except Exception as e:
-            LOG.warning(_LW("Get esx cookies failed: %s"), e)
+            LOG.warning(_LW("Get esx cookies failed: %s"), e,
+                        instance=vi.instance)
             dc_path = vutil.get_inventory_path(session.vim, vi.dc_info.ref)
 
             host = self._session._host
@@ -1191,7 +1192,7 @@ class VMwareVMOps(object):
             except Exception as excep:
                 LOG.warning(_LW("In vmwareapi:vmops:_destroy_instance, got "
                                 "this exception while un-registering the VM: "
-                                "%s"), excep)
+                                "%s"), excep, instance=instance)
             # Delete the folder holding the VM related content on
             # the datastore.
             if destroy_disks and vm_ds_path:
@@ -1214,7 +1215,8 @@ class VMwareVMOps(object):
                 except Exception:
                     LOG.warning(_LW("In vmwareapi:vmops:_destroy_instance, "
                                     "exception while deleting the VM contents "
-                                    "from the disk"), exc_info=True)
+                                    "from the disk"),
+                                exc_info=True, instance=instance)
         except exception.InstanceNotFound:
             LOG.warning(_LW('Instance does not exist on backend'),
                         instance=instance)
@@ -2268,7 +2270,8 @@ class VMwareVMOps(object):
                             str(sized_disk_ds_loc))
                 except Exception as e:
                     LOG.warning(_LW("Root disk file creation "
-                                    "failed - %s"), e)
+                                    "failed - %s"),
+                                e, instance=vi.instance)
                     with excutils.save_and_reraise_exception():
                         LOG.error(_LE('Failed to copy cached '
                                       'image %(source)s to '
@@ -2276,7 +2279,8 @@ class VMwareVMOps(object):
                                       '%(error)s'),
                                   {'source': vi.cache_image_path,
                                    'dest': sized_disk_ds_loc,
-                                   'error': e})
+                                   'error': e},
+                                  instance=vi.instance)
                         try:
                             ds_util.file_delete(self._session,
                                                 sized_disk_ds_loc,
