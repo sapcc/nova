f953448fda52df615c7e6970d61b6a5bb604c0dd
Fabian Wiesel <fabian.wiesel@sap.com>
2018-02-09 16:28:02 +0100

    Add uniqueness constraint to bdm mapping
    There can be only one block-device-mapping per instance-volume pair

--- nova/db/sqlalchemy/models.py
+++ nova/db/sqlalchemy/models.py
@@ -569,8 +569,8 @@ class BlockDeviceMapping(BASE, NovaBase, models.SoftDeleteMixin):
         Index('volume_id', 'volume_id'),
         Index('block_device_mapping_instance_uuid_device_name_idx',
               'instance_uuid', 'device_name'),
-        Index('block_device_mapping_instance_uuid_volume_id_idx',
-              'instance_uuid', 'volume_id'),
+        Index('block_device_mapping_instance_uuid_volume_id_deleted_idx',
+              'instance_uuid', 'volume_id', 'deleted', unique=True),
         Index('block_device_mapping_instance_uuid_idx', 'instance_uuid'),
     )
     id = Column(Integer, primary_key=True, autoincrement=True)
