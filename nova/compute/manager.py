eaa59d9d108df2cb807771f22598e7a3262913eb
mmidolesov <mmidolesov@vmware.com>
2018-03-20 04:17:14 -0700

    Merging affinity/anti-affinity from stable/mitaka-m3 to stable/queens-m3
    (cherry picked from commit 2afb07c48c528a76f34c0a98d0586cd325717dbd)

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -1258,7 +1258,7 @@ class ComputeManager(manager.Manager):
             # becomes problematic here then we should consider adding a config
             # option or a driver flag to tell us if we should thread this out
             # in the background on startup
-            self._error_out_instances_whose_build_was_interrupted(
+                      r_out_instances_whose_build_was_interrupted(
                 context, already_handled)
 
         finally:
@@ -1514,7 +1514,10 @@ class ComputeManager(manager.Manager):
                     max_server = rules['max_server_per_host']
                 else:
                     max_server = 1
-                if len(members_on_host) >= max_server:
+                resource_scheduling = self.driver.capabilities.get(
+                                        "resource_scheduling", False)
+                if len(members_on_host) >= max_server and not \
+                        resource_scheduling:
                     msg = _("Anti-affinity instance group policy "
                             "was violated.")
                     raise exception.RescheduledException(
