fdd90ad4cd04be0433b42b059374483f7d1ea878
mmidolesov <mmidolesov@vmware.com>
2018-04-03 04:57:16 -0700

    486ad55deaf2c7931ef23bc8e3f3290383fba4c0 Allow the network-driver to specify the bridge-name;

--- nova/network/neutronv2/api.py
+++ nova/network/neutronv2/api.py
@@ -2252,7 +2252,8 @@ class API(base_api.NetworkAPI):
         elif vif_type == network_model.VIF_TYPE_DVS:
             # The name of the DVS port group will contain the neutron
             # network id
-            bridge = port['network_id']
+            bridge = port_details.get(network_model.VIF_DETAILS_BRIDGE_NAME,
+                                      port['network_id'])
         elif (vif_type == network_model.VIF_TYPE_VHOSTUSER and
          port_details.get(network_model.VIF_DETAILS_VHOSTUSER_OVS_PLUG,
                           False)):
