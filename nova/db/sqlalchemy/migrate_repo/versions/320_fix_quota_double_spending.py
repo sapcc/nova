a7c2a57cb8abc42c363d4354ab069cabc461916c
mmidolesov <mmidolesov@vmware.com>
2018-04-05 01:38:32 -0700

    add a uniqueness constraint to the quota_usages table - b6490efe93ef645cda1336db691cc1d011fd9a3c id

---
+++ nova/db/sqlalchemy/migrate_repo/versions/320_fix_quota_double_spending.py
@@ -0,0 +1,19 @@
+from sqlalchemy import MetaData, Table
+from migrate.changeset.constraint import UniqueConstraint
+
+def _build_constraint(migrate_engine):
+    meta = MetaData()
+    meta.bind = migrate_engine
+    table = Table('quota_usages', meta, autoload=True)
+    cons = UniqueConstraint(
+        'project_id', 'user_id', 'resource', 'deleted',
+        table=table,
+    )
+
+def upgrade(migrate_engine):
+    cons = _build_constraint(migrate_engine)
+    cons.create()
+
+def downgrade(migrate_engine):
+    cons = _build_constraint(migrate_engine)
+    cons.drop()
