5321805e0764c963f1a1758a42438e197a133dde
Fabian Wiesel <fabian.wiesel@sap.com>
2018-12-05 16:24:11 +0100

    Fix hv_enabled extra-spec

--- nova/virt/vmwareapi/vm_util.py
+++ nova/virt/vmwareapi/vm_util.py
@@ -230,8 +230,9 @@ def get_vm_create_spec(client_factory, instance, data_store_name,
     config_spec.version = extra_specs.hw_version
 
     # Allow nested hypervisor instances to host 64 bit VMs.
-    if os_type in ("vmkernel5Guest", "vmkernel6Guest", "vmkernel65Guest",
-                   "windowsHyperVGuest"):
+    if (os_type in ("vmkernel5Guest", "vmkernel6Guest", "vmkernel65Guest",
+                   "windowsHyperVGuest") or
+            extra_specs.hv_enabled == "True"):
         config_spec.nestedHVEnabled = "True"
     # Append the profile spec
     if profile_spec:
