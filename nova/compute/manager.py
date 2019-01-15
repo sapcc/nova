e7fc551c2588ad113f54a07af53adc5f4cfc1b5a
Johannes Kulik <johannes.kulik@sap.com>
2019-01-15 11:37:04 +0100

    Fix pep8 for "flatten power_sync_state to intervall duration"

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -7156,7 +7156,8 @@ class ComputeManager(manager.Manager):
                 self._query_driver_power_state_and_sync(context, db_instance)
 
             try:
-                greenthread.sleep(random.randint(1, CONF.sync_power_state_interval))
+                sleep_time = random.randint(1, CONF.sync_power_state_interval)
+                greenthread.sleep(sleep_time)
                 query_driver_power_state_and_sync()
             except Exception:
                 LOG.exception("Periodic sync_power_state task had an "
