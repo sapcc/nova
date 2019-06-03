fc363852e3dccd201e53134d093ec58269b0b162
Johannes Kulik <johannes.kulik@sap.com>
2019-06-03 10:48:42 +0200

    Fix tests for graceful shutdown patches
    The graceful shutdown introduced an `eventlet.GreenPool` for spawning
    VMs. This broke some tests that assumed nova would spawn instances
    synchronously in the tests. We use nova tests' helper class to make the
    pool work in a synchronous manner.
    
    (cherry picked from commit 2b7974e3c19e663401853b6e78266f16dd904206)

--- nova/tests/unit/compute/test_compute_mgr.py
+++ nova/tests/unit/compute/test_compute_mgr.py
@@ -55,6 +55,7 @@ from nova.objects import migrate_data as migrate_data_obj
 from nova.objects import network_request as net_req_obj
 from nova import test
 from nova.tests import fixtures
+from nova.tests.unit.compute import eventlet_utils
 from nova.tests.unit.api.openstack import fakes
 from nova.tests.unit.compute import fake_resource_tracker
 from nova.tests.unit import fake_block_device
@@ -88,6 +89,7 @@ class ComputeManagerUnitTestCase(test.NoDBTestCase):
                                               fakes.FAKE_PROJECT_ID)
 
         self.useFixture(fixtures.SpawnIsSynchronousFixture())
+        self.compute.instance_running_pool = eventlet_utils.SyncPool()
         self.useFixture(fixtures.EventReporterStub())
 
     @mock.patch.object(manager.ComputeManager, '_get_power_state')
@@ -5375,6 +5377,7 @@ class ComputeManagerBuildInstanceTestCase(test.NoDBTestCase):
                                                        'fake-node']]}}
 
         self.useFixture(fixtures.SpawnIsSynchronousFixture())
+        self.compute.instance_running_pool = eventlet_utils.SyncPool()
 
         def fake_network_info():
             return network_model.NetworkInfo([{'address': '1.2.3.4'}])
@@ -5441,6 +5444,7 @@ class ComputeManagerBuildInstanceTestCase(test.NoDBTestCase):
     def test_build_and_run_instance_with_unlimited_max_concurrent_builds(self):
         self.flags(max_concurrent_builds=0)
         self.compute = manager.ComputeManager()
+        self.compute.instance_running_pool = eventlet_utils.SyncPool()
         self._test_build_and_run_instance()
 
     @mock.patch.object(objects.InstanceActionEvent,
@@ -7118,6 +7122,7 @@ class ComputeManagerMigrationTestCase(test.NoDBTestCase):
             status='migrating')
         self.migration.save = mock.MagicMock()
         self.useFixture(fixtures.SpawnIsSynchronousFixture())
+        self.compute.instance_running_pool = eventlet_utils.SyncPool()
         self.useFixture(fixtures.EventReporterStub())
 
     @contextlib.contextmanager
