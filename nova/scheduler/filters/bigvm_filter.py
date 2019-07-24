6ad6a2c0300b0a6d2d4f08e94b23cc4e6cf1db1a
Johannes Kulik <johannes.kulik@sap.com>
2019-07-24 14:19:51 +0200

    [scheduler] Make BigVmFilter more dynamic
    Instead of having a static threshold for the `BigVmFilter`, we compute
    the threshold depending on the hypervisor size and the requested RAM of
    the big VM. The reasoning behind this is, that on bigger hypervisors a
    small big VM doesn't matter as much as a big big VM, e.g. 1.4 TB on a 6
    TB hypervisor vs. 5.9 TB on a 6 TB hypervisor. We basically aim at 50 %
    of the requested RAM free on average over all hosts in the cluster.
    
    The filter thus currently only works if a compute-node is in a host
    aggregate defining the `_AGGREGATE_KEY` (currently "hv_size_mb") having
    an integer defining the hypervisor's RAM size as a value.

--- nova/scheduler/filters/bigvm_filter.py
+++ nova/scheduler/filters/bigvm_filter.py
@@ -2,31 +2,73 @@ from oslo_log import log as logging
 
 import nova.conf
 from nova.scheduler import filters
+from nova.scheduler.filters import utils
 
 LOG = logging.getLogger(__name__)
 
 CONF = nova.conf.CONF
 
+_AGGREGATE_KEY = 'hv_size_mb'
 
 class BigVmFilter(filters.BaseHostFilter):
+    """Only schedule big VMs to a vSphere cluster (i.e. nova-compute host) if
+    the memory-utilization of the cluster is below a threshold depending on the
+    hypervisor size and the requested memory.
+    """
 
     RUN_ON_REBUILD = False
 
+    def _get_max_ram_percent(self, requested_ram_mb, hypervisor_ram_mb):
+        """
+        We want the hosts to have on average half the requested memory free.
+        """
+        requested_ram_mb = float(requested_ram_mb)
+        hypervisor_ram_mb = float(hypervisor_ram_mb)
+        hypervisor_max_used_ram_mb = hypervisor_ram_mb - requested_ram_mb / 2
+        return hypervisor_max_used_ram_mb / hypervisor_ram_mb * 100
+
     def host_passes(self, host_state, spec_obj):
         requested_ram_mb = spec_obj.memory_mb
+        # not scheduling a big VM -> every host is fine
         if requested_ram_mb < CONF.bigvm_mb:
             return True
 
         free_ram_mb = host_state.free_ram_mb
         total_usable_ram_mb = host_state.total_usable_ram_mb
         used_ram_mb = total_usable_ram_mb - free_ram_mb
-        used_ram_percent = used_ram_mb / total_usable_ram_mb * 100
-        max_ram_percent = CONF.scheduler.bigvm_memory_utilization_max
+        used_ram_percent = float(used_ram_mb) / total_usable_ram_mb * 100.0
+
+        # get the hypervisor size from the host aggregate
+        metadata = utils.aggregate_metadata_get_by_host(host_state)
+        aggregate_vals = metadata.get(_AGGREGATE_KEY, None)
+        if not aggregate_vals:
+            LOG.error("%(host_state)s does not have %(aggregate_key)s set so "
+                      "probably isn't assigned to a hypervisor-size host "
+                      "aggregate, which prevents big VM scheduling.",
+                      {'host_state': host_state,
+                       'aggregate_key': _AGGREGATE_KEY})
+            return False
+
+        # there should be only one value anyways ...
+        aggregate_val = list(aggregate_vals)[0]
+        try:
+            hypervisor_ram_mb = int(aggregate_val)
+        except ValueError:
+            LOG.error("%(host_state)s has an invalid value for "
+                          "%(aggregate_key)s: %(aggregate_val)s. Only "
+                          "integers are supported.",
+                          {'host_state': host_state,
+                           'aggregate_key': _AGGREGATE_KEY,
+                           'aggregate_val': aggregate_val})
+            return False
+
+        max_ram_percent = self._get_max_ram_percent(requested_ram_mb,
+                                                    hypervisor_ram_mb)
 
         if used_ram_percent > max_ram_percent:
-            LOG.debug("%(host_state)s does not have less than "
-                      "%(max_ram_percent)s % RAM utilization (has "
-                      "%(used_ram_percent)s %) and is thus not suitable "
+            LOG.info("%(host_state)s does not have less than "
+                      "%(max_ram_percent)s %% RAM utilization (has "
+                      "%(used_ram_percent)s %%) and is thus not suitable "
                       "for big VMs.",
                       {'host_state': host_state,
                        'max_ram_percent': max_ram_percent,
---
+++ nova/tests/unit/scheduler/filters/test_bigvm_filters.py
@@ -0,0 +1,97 @@
+import mock
+
+import nova.conf
+from nova import objects
+from nova.scheduler.filters import bigvm_filter
+from nova import test
+from nova.tests.unit.scheduler import fakes
+
+CONF = nova.conf.CONF
+
+@mock.patch('nova.scheduler.filters.utils.aggregate_metadata_get_by_host')
+class TestBigVmFilter(test.NoDBTestCase):
+
+    def setUp(self):
+        super(TestBigVmFilter, self).setUp()
+        self.hv_size = CONF.bigvm_mb + 1024
+        self.filt_cls = bigvm_filter.BigVmFilter()
+
+    def test_big_vm_with_small_vm_passes(self, agg_mock):
+        spec_obj = objects.RequestSpec(
+            flavor=objects.Flavor(memory_mb=1024))
+        host = fakes.FakeHostState('host1', 'compute', {})
+        self.assertTrue(self.filt_cls.host_passes(host, spec_obj))
+
+    def test_big_vm_without_aggregate(self, agg_mock):
+        agg_mock.return_value = {}
+        spec_obj = objects.RequestSpec(
+            flavor=objects.Flavor(memory_mb=CONF.bigvm_mb))
+        host = fakes.FakeHostState('host1', 'compute',
+                {'free_ram_mb': self.hv_size,
+                 'total_usable_ram_mb': self.hv_size})
+        self.assertFalse(self.filt_cls.host_passes(host, spec_obj))
+
+    def test_big_vm_with_non_int_aggregate(self, agg_mock):
+        agg_mock.return_value = {'hv_size_mb': ['foo']}
+        spec_obj = objects.RequestSpec(
+            flavor=objects.Flavor(memory_mb=CONF.bigvm_mb))
+        host = fakes.FakeHostState('host1', 'compute',
+                {'free_ram_mb': self.hv_size,
+                 'total_usable_ram_mb': self.hv_size})
+        self.assertFalse(self.filt_cls.host_passes(host, spec_obj))
+
+    def test_big_vm_without_enough_ram(self, agg_mock):
+        # there's enough RAM available in the cluster but not enough (~50 % of
+        # the requested size on average
+        # 12 hosts (bigvm + 1 GB size)
+        # 11 big VM + some smaller (12 * 1 GB) already deployed
+        # -> still bigvm_mb left, but ram utilization ratio of all hosts is too
+        # high
+        agg_mock.return_value = {'hv_size_mb': [self.hv_size]}
+        spec_obj = objects.RequestSpec(
+            flavor=objects.Flavor(memory_mb=CONF.bigvm_mb))
+        total_ram = self.hv_size * 12
+        host = fakes.FakeHostState('host1', 'compute',
+                {'free_ram_mb': CONF.bigvm_mb,
+                 'total_usable_ram_mb': total_ram})
+        self.assertFalse(self.filt_cls.host_passes(host, spec_obj))
+
+    def test_big_vm_without_enough_ram_ignores_ram_ratio(self, agg_mock):
+        # same as test_big_vm_without_enough_ram but with more theoretical RAM
+        # via `ram_allocation_ratio`. big VMs reserve all memory so the ratio
+        # does not count for them.
+        agg_mock.return_value = {'hv_size_mb': [self.hv_size]}
+        spec_obj = objects.RequestSpec(
+            flavor=objects.Flavor(memory_mb=CONF.bigvm_mb))
+        total_ram = self.hv_size * 12
+        host = fakes.FakeHostState('host1', 'compute',
+                {'free_ram_mb': CONF.bigvm_mb,
+                 'total_usable_ram_mb': total_ram,
+                 'ram_allocation_ratio': 1.5})
+        self.assertFalse(self.filt_cls.host_passes(host, spec_obj))
+
+    def test_big_vm_without_enough_ram_percent(self, agg_mock):
+        # there's just closely not enough RAM available
+        agg_mock.return_value = {'hv_size_mb': [self.hv_size]}
+        spec_obj = objects.RequestSpec(
+            flavor=objects.Flavor(memory_mb=CONF.bigvm_mb))
+        total_ram = self.hv_size * 12
+        hv_percent = self.filt_cls._get_max_ram_percent(CONF.bigvm_mb,
+                                                        self.hv_size)
+        free_ram_mb = total_ram - (total_ram * hv_percent / 100.0) - 128
+        host = fakes.FakeHostState('host1', 'compute',
+                {'free_ram_mb': free_ram_mb,
+                 'total_usable_ram_mb': total_ram})
+        self.assertFalse(self.filt_cls.host_passes(host, spec_obj))
+
+    def test_big_vm_with_enough_ram(self, agg_mock):
+        agg_mock.return_value = {'hv_size_mb': [self.hv_size]}
+        spec_obj = objects.RequestSpec(
+            flavor=objects.Flavor(memory_mb=CONF.bigvm_mb))
+        total_ram = self.hv_size * 12
+        hv_percent = self.filt_cls._get_max_ram_percent(CONF.bigvm_mb,
+                                                        self.hv_size)
+        host = fakes.FakeHostState('host1', 'compute',
+                {'free_ram_mb': total_ram - (total_ram * hv_percent / 100.0),
+                 'total_usable_ram_mb': total_ram})
+        self.assertTrue(self.filt_cls.host_passes(host, spec_obj))
