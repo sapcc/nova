06f32375932c3cb018c18209b283523352ae94c8
Matt Riedemann <mriedem.os@gmail.com>
2019-08-09 17:24:07 -0400

    Add functional regression recreate test for bug 1839560
    This adds a functional test which recreates bug 1839560
    where the driver reports a node, then no longer reports
    it so the compute manager deletes it, and then the driver
    reports it again later (this can be common with ironic
    nodes as they undergo maintenance). The issue is that since
    Ia69fabce8e7fd7de101e291fe133c6f5f5f7056a in Rocky, the
    ironic node uuid is re-used for the compute node uuid but
    there is a unique constraint on the compute node uuid so
    when trying to create the compute node once the ironic node
    is available again, the compute node create fails with a
    duplicate entry error due to the duplicate uuid. To recreate
    this in the functional test, a new fake virt driver is added
    which provides a predictable uuid per node like the ironic
    driver. The test also shows that archiving the database is
    a way to workaround the bug until it's properly fixed.
    
    Change-Id: If822509e906d5094f13a8700b2b9ed3c40580431
    Related-Bug: #1839560

--- nova/virt/fake.py
+++ nova/virt/fake.py
@@ -27,6 +27,7 @@ import collections
 import contextlib
 import copy
 import time
+import uuid
 
 from oslo_log import log as logging
 from oslo_serialization import jsonutils
@@ -713,3 +714,16 @@ class FakeLiveMigrateDriver(FakeDriver):
     def live_migration_abort(self, instance):
         self._abort_migration = True
         self._migrating = False
+
+
+class PredictableNodeUUIDDriver(SmallFakeDriver):
+    """SmallFakeDriver variant that reports a predictable node uuid in
+    get_available_resource, like IronicDriver.
+    """
+    def get_available_resource(self, nodename):
+        resources = super(
+            PredictableNodeUUIDDriver, self).get_available_resource(nodename)
+        # This is used in ComputeNode.update_from_virt_driver which is called
+        # from the ResourceTracker when creating a ComputeNode.
+        resources['uuid'] = uuid.uuid5(uuid.NAMESPACE_DNS, nodename)
+        return resources
