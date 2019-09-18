e1374f77db9b877e8310e6046ad426b263c9890a
Johannes Kulik <johannes.kulik@sap.com>
2019-09-18 11:44:09 +0200

    [api] Prohibit certain VMs from joining server groups
    We cannot adhere to affinity-rules if we handle big VMs differently on
    the cluster or when spawning smaller VMs on specifically prepared hosts.
    We could just ignore that, but we want the user to know about it.
    Therefore, we prohibit big VMs from being created, if the
    creation-request contains a server group.

--- nova/tests/unit/api/openstack/compute/test_serversV21.py
+++ nova/tests/unit/api/openstack/compute/test_serversV21.py
@@ -3629,6 +3629,26 @@ class ServersControllerCreateTest(test.TestCase):
         self.assertRaises(exception.ValidationError,
                           self.controller.create, self.req, body=self.body)
 
+    def test_create_instance_with_group_hint_big_vm(self):
+        ctxt = self.req.environ['nova.context']
+        test_group = objects.InstanceGroup(ctxt)
+        test_group.project_id = ctxt.project_id
+        test_group.user_id = ctxt.user_id
+        test_group.create()
+
+        CONF.set_override('bigvm_mb', 512)
+
+        def fake_instance_destroy(context, uuid, constraint):
+            return fakes.stub_instance(1)
+
+        self.stub_out('nova.db.instance_destroy', fake_instance_destroy)
+        self.body['os:scheduler_hints'] = {'group': test_group.uuid}
+        self.req.body = jsonutils.dump_as_bytes(self.body)
+        self.assertRaisesRegex(webob.exc.HTTPBadRequest,
+                               r'because of its scheduling requirements',
+                               self.controller.create, self.req,
+                               body=self.body)
+
     def test_create_instance_with_neutronv2_port_in_use(self):
         network = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
         port = 'eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee'
