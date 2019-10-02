da72e73dabc5513dfee6572b89bf02252686f3f7
Johannes Kulik <johannes.kulik@sap.com>
2019-10-02 14:18:03 +0200

    [api] Don't remove ANSI from console output
    As the comment states, it's only relevant for XML and should be removed
    in the future. The future is now, as we want fully colored logs :D

--- nova/api/openstack/compute/console_output.py
+++ nova/api/openstack/compute/console_output.py
@@ -60,11 +60,4 @@ class ConsoleOutputController(wsgi.Controller):
         except NotImplementedError:
             common.raise_feature_not_supported()
 
-        # XML output is not correctly escaped, so remove invalid characters
-        # NOTE(cyeoh): We don't support XML output with V2.1, but for
-        # backwards compatibility reasons we continue to filter the output
-        # We should remove this in the future
-        remove_re = re.compile('[\x00-\x08\x0B-\x1F]')
-        output = remove_re.sub('', output)
-
         return {'output': output}
