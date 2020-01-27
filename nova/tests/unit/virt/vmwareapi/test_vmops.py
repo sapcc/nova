4d8e0fbe7095c839cc166e99125158d3e90f59cb
mmidolesov <mmidolesov@vmware.com>
2020-01-27 14:55:55 +0000

    Fix unit tests for spawn VMs on prepared empty host
    This is regarding the change in _get_server_groups in inside
    vm_util.py where we are checking if instance needs special
    spawning.
    Commit - 5c80f53b

--- nova/tests/unit/virt/vmwareapi/test_vmops.py
+++ nova/tests/unit/virt/vmwareapi/test_vmops.py
@@ -1573,6 +1573,7 @@ class VMwareVMOpsTestCase(test.TestCase):
         recorded_methods = [c[1][1] for c in mock_call_method.mock_calls]
         self.assertEqual(expected_methods, recorded_methods)
 
+    @mock.patch('nova.virt.vmwareapi.vm_util.vm_needs_special_spawning')
     @mock.patch.object(vmops.VMwareVMOps, '_create_folders',
                        return_value='fake_vm_folder')
     @mock.patch(
@@ -1615,10 +1616,10 @@ class VMwareVMOpsTestCase(test.TestCase):
                    mock_configure_config_drive,
                    mock_update_vnic_index,
                    mock_create_folders,
+                   mock_special_spawning,
                    block_device_info=None,
                    extra_specs=None,
                    config_drive=False):
-
         if extra_specs is None:
             extra_specs = vm_util.ExtraSpecs()
 
@@ -2051,9 +2052,11 @@ class VMwareVMOpsTestCase(test.TestCase):
     def test_create_swap_with_no_bdi(self):
         self._test_create_swap_from_instance(None)
 
+    @mock.patch('nova.virt.vmwareapi.vm_util.vm_needs_special_spawning')
     @mock.patch.object(vmops.VMwareVMOps, '_create_folders',
                        return_value='fake_vm_folder')
-    def test_build_virtual_machine(self, mock_create_folder):
+    def test_build_virtual_machine(self, mock_create_folder,
+                                   mock_special_spawning):
         image_id = nova.tests.unit.image.fake.get_valid_image_id()
         image = images.VMwareImage(image_id=image_id)
 
