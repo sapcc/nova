1f5f7f5070dff21a3dbe12ea847409c438bac8ff
mmidolesov <mmidolesov@vmware.com>
2020-02-01 06:54:06 +0000

    Fix tests for "Move freeing a host for spawning in own service"
    Update NotificationPublisher version in test_notification.py

--- nova/tests/unit/notifications/objects/test_notification.py
+++ nova/tests/unit/notifications/objects/test_notification.py
@@ -402,7 +402,7 @@ notification_object_data = {
     'IpPayload': '1.0-8ecf567a99e516d4af094439a7632d34',
     'KeypairNotification': '1.0-a73147b93b520ff0061865849d3dfa56',
     'KeypairPayload': '1.0-6daebbbde0e1bf35c1556b1ecd9385c1',
-    'NotificationPublisher': '2.2-b6ad48126247e10b46b6b0240e52e614',
+    'NotificationPublisher': '2.2-ff8ef16673817ca7a3ea69c689e260c6',
     'ServerGroupNotification': '1.0-a73147b93b520ff0061865849d3dfa56',
     'ServerGroupPayload': '1.0-eb4bd1738b4670cfe1b7c30344c143c3',
     'ServiceStatusNotification': '1.0-a73147b93b520ff0061865849d3dfa56',
