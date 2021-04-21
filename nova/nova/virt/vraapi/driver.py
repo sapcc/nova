b8f77f2324e46b8109b709c2f7d7ef8db8b53d8a
Martin Midolesov (c) <mmidolesov@vmware.com>
2021-04-21 11:02:14 +0300

    Rename config value file

--- nova/nova/virt/vraapi/driver.py
+++ nova/nova/virt/vraapi/driver.py
@@ -26,7 +26,7 @@ import nova.privsep.path
 from nova.virt import hardware
 from nova.virt.vraapi import machine
 from nova.virt.vraapi import vraops
-from config import volume_config
+from config import vra_config
 
 LOG = logging.getLogger(__name__)
 
