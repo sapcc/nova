ed6591558abc3bfb5444659312044ba3a4f37962
Johannes Kulik <johannes.kulik@sap.com>
2021-09-16 14:48:24 +0200

    Add a way to trigger a server-group sync on the driver
    We're going to change the API to allow updates of server-group members
    and need to trigger a sync in the backend, whenever such a change
    occurs. Therefore, we need a bunch of methods going through the whole
    stack to call the driver.
    
    We use a cast and not a call here, because we cannot let the API wait
    for all of this to happen. If the cast gets lost somehow, a sync-loop
    implemented in the driver will pick the change up, eventually
    
    The API will have to supply a list of hosts to call, so we only sync the
    group on the necessary hosts.
    
    Change-Id: I00e012ed52ba9fd36b094ecf2dc86b023f2f5a21
    (cherry picked from commit f3d2ad7e457ab0e47e63c82f09f91572ba04711f)
    (cherry picked from commit ae57baaf366c607a26b3bc5059a348bb7453f536)

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -6098,6 +6098,10 @@ class API:
         self.compute_rpcapi.deny_share(
             context, instance, share_mapping)
 
+    def sync_server_group(self, context, hosts, sg_uuid):
+        for host in hosts:
+            self.compute_rpcapi.sync_server_group(context, host, sg_uuid)
+
 
 def target_host_cell(fn):
     """Target a host-based function to a cell.
