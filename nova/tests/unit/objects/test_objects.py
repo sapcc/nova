8594130def71b75cd57680daa0d1185058e33dd2
Fabian Wiesel <fabian.wiesel@sap.com>
2021-10-04 16:00:14 +0200

    Vmwareapi: Live-migration changes on conductor level.
    Split out the changes for live-migration which are affecting the conductor
    Basing it on the upstream VMWareLiveMigrateData 1.0 version in the hope
    that we have an easier path merging the code with upstream eventually
    
    Change-Id: I5d8417c836d735122e033eeb72f7671b2558afc4
    (cherry picked from commit a2c335fd4c51ad26776a73fe52b22b07359f5f9d)
    (cherry picked from commit 6ee8e60a1473d0bd5e3dd55860f1afb05f8a9ed2)

--- nova/tests/unit/objects/test_objects.py
+++ nova/tests/unit/objects/test_objects.py
@@ -1183,7 +1183,7 @@ object_data = {
     'VirtCPUTopology': '1.0-fc694de72e20298f7c6bab1083fd4563',
     'VirtualInterface': '1.3-efd3ca8ebcc5ce65fff5a25f31754c54',
     'VirtualInterfaceList': '1.0-9750e2074437b3077e46359102779fc6',
-    'VMwareLiveMigrateData': '1.1-14a63b00f1b8880453df6d19a883bc73',
+    'VMwareLiveMigrateData': '1.2-69ac58f0c1f20686ddfdfae03ceb0929',
     'VolumeUsage': '1.0-6c8190c46ce1469bb3286a1f21c2e475',
     'XenDeviceBus': '1.0-272a4f899b24e31e42b2b9a7ed7e9194',
 }
