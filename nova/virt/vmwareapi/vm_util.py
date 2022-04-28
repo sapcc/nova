c72380c68660d252473a38777b6b6da08abbd5d7
Fabian Wiesel <fabian.wiesel@sap.com>
2022-04-28 12:24:43 +0200

    Vmwareapi: Move pre_live_migration to vmops
    Most code related to vms is in vmops, not in the driver
    So we move this code there too
    
    Change-Id: I1b801c8f12b377dd74a31ef646216c564631fe7f
    (cherry picked from commit ade6f4c49aa74cf3145a33df3c68855d816b0ab4)
    (cherry picked from commit ffcdef1cb1bb7c8eb95f9892265db26e8d5ecbb6)

--- nova/virt/vmwareapi/vm_util.py
+++ nova/virt/vmwareapi/vm_util.py
@@ -899,7 +899,8 @@ def get_hardware_devices_by_type(session, vm_ref):
     nics = {}
     controllers = {}
     disks = {}
-    swap = []
+    cdroms = {}
+    swap = None
     ephemeral = []
     first_device = None
 
@@ -921,7 +922,7 @@ def get_hardware_devices_by_type(session, vm_ref):
             backing_type = device.backing.__class__.__name__
             if backing_type == "VirtualDiskFlatVer2BackingInfo":
                 if "swap" in device.backing.fileName:
-                    swap.append(device)
+                    swap = device
                 elif "ephemeral" in device.backing.fileName:
                     ephemeral.append(device)
                 else:
@@ -936,6 +937,8 @@ def get_hardware_devices_by_type(session, vm_ref):
                     disks[uuid.lower()] = device
             elif backing_type == "VirtualDiskRawDiskMappingVer1BackingInfo":
                 disks[device.backing.lunUuid.lower()] = device
+        elif device_type == "VirtualCdrom":
+            cdroms[device.backing.fileName] = device
 
     return {
         "nics": nics,
@@ -943,6 +946,7 @@ def get_hardware_devices_by_type(session, vm_ref):
         "swap": swap,
         "ephemeral": ephemeral,
         "disks": disks,
+        "cdroms": cdroms,
         "root_device": first_device
     }
 
