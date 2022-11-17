0c9fb335a4a2af2cae8bdc5082e265495bc0af18
Johannes Kulik <johannes.kulik@sap.com>
2022-11-17 11:15:03 +0100

    vmware: Integer division Python 2 -> 3 fix
    In Python 2, division with the / operator results in an integer-type
    result if divisor and dividend are integer-typed - if one of them is of
    type float, then the result will be a float.
    
    Python 3 made this more explicit by introducing the // operator which
    always results in an integer result while changing the / operator to
    always return floats.
    
    Some of our code relied on the old behavior and we need to update it to
    use // so we don't put floats to placement or the vCenter where they
    don't understand them.
    
    There's even some code that relied on the new behavior and newer worked
    before - we don't update it here.
    
    Change-Id: Ib81728cc8dcde852a035bfbbd380435ed06c56ba
    (cherry picked from commit 4b996abe79b226180cb2619ac0bc7897178fa6ac)

--- nova/tests/unit/virt/vmwareapi/test_driver_api.py
+++ nova/tests/unit/virt/vmwareapi/test_driver_api.py
@@ -2271,10 +2271,10 @@ class VMwareAPIVMTestCase(test.NoDBTestCase,
                 'allocation_ratio': CONF.initial_ram_allocation_ratio,
             },
             orc.DISK_GB: {   # Fix this
-                'total': 1024.0,
+                'total': 1024,
                 'reserved': 0,
                 'min_unit': 1,
-                'max_unit': 500.0,
+                'max_unit': 500,
                 'step_size': 1,
                 'allocation_ratio': CONF.initial_disk_allocation_ratio,
             },
@@ -2291,6 +2291,8 @@ class VMwareAPIVMTestCase(test.NoDBTestCase,
         traits = self.pt.data(self.node_name).traits
         # nova with vmware only supports HW_ARCH_X86_64
         self.assertEqual(traits, {'HW_ARCH_X86_64'})
+        self.assertIsInstance(inventory[orc.DISK_GB]['total'], int)
+        self.assertIsInstance(inventory[orc.DISK_GB]['max_unit'], int)
 
     def test_invalid_datastore_regex(self):
 
