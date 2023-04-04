dd37a935111c9fde86c8a66bc91ac6fb1f8dae3b
Johannes Kulik <johannes.kulik@sap.com>
2023-04-04 11:59:54 +0200

    Support targeting an ESXi during live-migration
    Since the VMware driver manages a cluster of ESXi hosts, targeting a
    cluster during live-migration is not always enough. There are cases -
    namely big VMs - where we want the VM to be moved to a specific ESXi
    during the live migration.
    
    To support this, we extend the `os-migrateLive` action to accept a
    `host_ref` argument starting with API version 2.34. We decided on 2.34,
    because with that version the live-migration becomes an asynchronous
    action. The `host_ref` argument must be a valid ManagedObjectReference
    of an ESXi node of the target cluster and can only be used together with
    a set `host` argument.
    
    The decisions to use the MoRef came from a simplicity point. We need to
    translate the MoRef to a node name here, but given a node name, we'd
    also have to translate to a MoRef. Looking up the name of a MoRef seemed
    the simpler operation, so we chose that as a start. It should not be
    hard to add node-name support in addition.
    
    Since no other drivers manage nodes, this is a "vmwareapi" specific
    extension of the API and probably not easily upstreamable.
    
    Change-Id: I5c2380dcc9f3fcb9a6f58413f93ef3479c873ff1
    (cherry picked from commit c194921eeaf26e10d8d7364c5f0d5970026c7a71)

--- nova/api/openstack/compute/migrate_server.py
+++ nova/api/openstack/compute/migrate_server.py
@@ -92,7 +92,8 @@ class MigrateServerController(wsgi.Controller):
     @wsgi.action('os-migrateLive')
     @validation.schema(schema.migrate_live, "2.0", "2.24")
     @validation.schema(schema.migrate_live_v2_25, "2.25", "2.29")
-    @validation.schema(schema.migrate_live_v2_30, "2.30", "2.67")
+    @validation.schema(schema.migrate_live_v2_30, "2.30", "2.33")
+    @validation.schema(schema.migrate_live_v2_34, "2.34", "2.67")
     @validation.schema(schema.migrate_live_v2_68, "2.68")
     @validation.response_body_schema(schema.migrate_live_response)
     def _migrate_live(self, req, id, body):
@@ -109,6 +110,13 @@ class MigrateServerController(wsgi.Controller):
                     target={'project_id': instance.project_id})
 
         host = body["os-migrateLive"]["host"]
+        host_ref = None
+        if api_version_request.is_supported(req, min_version='2.34'):
+            # we might get a host_ref passed in addition to a host
+            host_ref = body["os-migrateLive"].get("host_ref")
+            if not host and host_ref:
+                msg = "Passing 'host_ref' requires passing 'host'"
+                raise exc.HTTPBadRequest(explanation=msg)
         block_migration = body["os-migrateLive"]["block_migration"]
         force = None
         async_ = api_version_request.is_supported(req, min_version='2.34')
@@ -132,7 +140,7 @@ class MigrateServerController(wsgi.Controller):
         try:
             self.compute_api.live_migrate(context, instance, block_migration,
                                           disk_over_commit, host, force,
-                                          async_)
+                                          async_, host_ref=host_ref)
         except (exception.NoValidHost,
                 exception.ComputeServiceUnavailable,
                 exception.InvalidHypervisorType,
