73e266f216d4940735bff8065a1520783239aa6a
Philipp Matthes <p.matthes@sap.com>
2025-07-08 18:28:52 +0200

    External scheduler: don't log credentials
    The external scheduler call logged two types of credentials:
    1. DB and mqtt connection secrets nested in the cell mapping
    2. The user's auth token from the request context
    This change removes the credentials from logging and also
    avoids sending them to the external scheduler.

--- nova/scheduler/external.py
+++ nova/scheduler/external.py
@@ -63,11 +63,22 @@ def call_external_scheduler_api(context, weighed_hosts, weights, spec_obj):
         return weighed_hosts
     timeout = CONF.filter_scheduler.external_scheduler_timeout
 
+    # Serialize the request spec and context to plain dicts, but remove
+    # sensitive information.
+    spec_data = spec_obj.obj_to_primitive()
+    obj_key = "nova_object.data"  # Used by obj_to_primitive to wrap objects.
+    if "requested_destination" in (unwrapped := spec_data[obj_key]):
+        req_destination = unwrapped["requested_destination"]
+        if "cell" in (unwrapped := req_destination[obj_key]):
+            del unwrapped["cell"][obj_key]["database_connection"]
+            del unwrapped["cell"][obj_key]["transport_url"]
+    context_data = context.to_dict()
+    if "auth_token" in context_data:
+        del context_data["auth_token"]
+
     json_data = {
-        "spec": spec_obj.obj_to_primitive(),
-        # Also serialize the request context, which contains the global request
-        # id and other information helpful for logging and request tracing.
-        "context": context.to_dict(),
+        "spec": spec_data,
+        "context": context_data,
         # Extract some flags from the spec to indicate the type of
         # request. This will allow the external scheduler to quickly
         # decide if it wants to handle the request or not.
