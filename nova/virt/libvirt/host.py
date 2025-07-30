a542044b237894405f8e792ef0fc74140cc5c08a
Stefan Kober <stefan.kober@cyberus-technology.de>
2025-07-30 13:35:02 +0200

    libvirt/ch: allow NUMA tunings with CHV
    Currently, the Nova Libvirt driver only enables numa tuning if QEMU/KVM
    is the hypervisor type. As we have built NUMA support for the Libvirt
    CHV driver, we now want to enable it in Nova too.
    
    Change-Id: Id0aca47471264cd217c4ee9fd25c8dcdba7a2cf7
    Signed-off-by: Stefan Kober <stefan.kober@cyberus-technology.de>

--- nova/virt/libvirt/host.py
+++ nova/virt/libvirt/host.py
@@ -85,6 +85,7 @@ CONF = nova.conf.CONF
 # This list is for libvirt hypervisor drivers that need special handling.
 # This is *not* the complete list of supported hypervisor drivers.
 HV_DRIVER_QEMU = "QEMU"
+HV_DRIVER_CH = "CH"
 
 SEV_KERNEL_PARAM_FILE = '/sys/module/kvm_amd/parameters/sev'
 
