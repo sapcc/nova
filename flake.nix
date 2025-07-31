6179ee44093f5c0877465409eed0d3dae7216bd3
Stefan Kober <stefan.kober@cyberus-technology.de>
2025-07-31 10:47:18 +0200

    nix: support openstack-nix tests
    On-behalf-of: SAP stefan.kober@sap.com

---
+++ flake.nix
@@ -0,0 +1,112 @@
+{
+  description = "Cloud Hypervisor driver for OpenStack Nova";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    openstack-nix = {
+      # url = "git+file:<path/to/openstack-nix>";
+      url = "git+https://github.com/cobaltcore-dev/openstack-nix.git";
+      # We have observed problems if nixpkgs of the consuming project and
+      # nixpkgs of openstack-nix are diverged too much. Therefore, use the
+      # nixpkgs of the consuming project. But take in mind, that openstack-nix
+      # currently isn't compatible with nixpkgs 25.05 or higher. See:
+      # https://github.com/cobaltcore-dev/openstack-nix/issues/8
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    cloud-hypervisor-src = {
+      url = "github:cyberus-technology/cloud-hypervisor?ref=gardenlinux";
+      flake = false;
+    };
+    # Nix tooling to build cloud-hypervisor.
+    crane.url = "github:ipetkov/crane/master";
+    # Get proper Rust toolchain, independent of pkgs.rustc.
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    libvirt-custom = {
+      url = "git+https://github.com/cyberus-technology/libvirt.git?ref=gardenlinux&submodules=1";
+      # url = "git+file:<path/to/libvirt>?submodules=1";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      openstack-nix,
+      cloud-hypervisor-src,
+      libvirt-custom,
+      crane,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (_: prev: {
+              cloud-hypervisor = pkgs.callPackage ./chv.nix {
+                inherit cloud-hypervisor-src;
+                craneLib = crane.mkLib pkgs;
+                rustToolchain = rust-bin.stable.latest.default;
+                cloud-hypervisor-meta = prev.cloud-hypervisor.meta;
+              };
+            })
+          ];
+        };
+        rust-bin = (rust-overlay.lib.mkRustBin { }) pkgs;
+        nixosModules = openstack-nix.nixosModules.${system};
+        openstackPackages = openstack-nix.packages.${system};
+        generateRootwrapConf = openstack-nix.lib.${system}.generateRootwrapConf;
+
+        novaSrc = pkgs.lib.cleanSourceWith {
+          src = ./.;
+          filter = path: type:
+            let
+              baseName = baseNameOf path;
+            in
+              # Exclude .nix files and nix/ directory
+              !(pkgs.lib.hasSuffix ".nix" baseName) &&
+              !(baseName == "nix" && type == "directory");
+        };
+
+        # The PBR setup does not work on the plain source code because no
+        # package version can be determined.
+        # We add a PKG-INFO file with the missing information to make it work.
+        # We use the version info of the original Nova package from
+        # openstack-nix.
+        fixedNovaSrc = pkgs.runCommand "add-package-info" { } ''
+          mkdir -p $out
+
+          cp -r ${novaSrc}/. $out
+
+          cat >$out/PKG-INFO <<EOL
+          Metadata-Version: 2.1
+          Name: nova
+          Version: ${openstackPackages.nova.version}
+          EOL
+        '';
+
+        novaPkg = openstackPackages.nova.overrideAttrs (_: {
+          src = fixedNovaSrc;
+          doInstallCheck = false;
+        });
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+        tests = import ./nix/tests/default.nix {
+          inherit
+            pkgs
+            nixosModules
+            novaPkg
+            generateRootwrapConf
+            libvirt-custom
+            ;
+        };
+      }
+    );
+}
---
+++ nix/tests/nova-chv-driver.nix
@@ -0,0 +1,285 @@
+{
+  pkgs,
+  nixosModules,
+  novaPkg,
+  generateRootwrapConf,
+  libvirt-custom,
+}:
+let
+  nova_env = pkgs.python3.buildEnv.override {
+    extraLibs = [ novaPkg ];
+  };
+  execDirs = pkgs.buildEnv {
+    name = "utils";
+    paths = [ nova_env ];
+  };
+  rootwrapConf = generateRootwrapConf {
+    package = nova_env;
+    filterPath = "/etc/nova/rootwrap.d";
+    execDirs = execDirs;
+  };
+in
+pkgs.nixosTest {
+  name = "OpenStack Cloud Hypervisor driver test";
+
+  nodes.controllerVM =
+    { ... }:
+    {
+      imports = [
+        nixosModules.controllerModule
+        nixosModules.testModules.testController
+      ];
+
+      nova.novaPackage = novaPkg;
+    };
+
+  nodes.computeVM =
+    { config, ... }:
+    {
+      imports = [
+        nixosModules.computeModule
+        nixosModules.testModules.testCompute
+      ];
+
+      virtualisation.libvirtd.package = pkgs.libvirt.overrideAttrs (_: {
+        src = libvirt-custom;
+        doInstallCheck = false;
+        doCheck = false;
+        patches = [ ./0001-meson-patch-in-an-install-prefix-for-building-on-nix.patch ./0002-substitute-zfs-and-zpool-commands.patch ];
+      });
+      systemd.services.virtchd.wantedBy = [ "multi-user.target" ];
+      systemd.services.virtlogd.wantedBy = [ "multi-user.target" ];
+      systemd.services.virtnodedevd.wantedBy = [ "multi-user.target" ];
+      systemd.tmpfiles.settings =
+        let
+          chv-firmware = pkgs.fetchurl {
+            url = "https://github.com/cloud-hypervisor/rust-hypervisor-firmware/releases/download/0.5.0/hypervisor-fw";
+            hash = "sha256-Sgoel3No9rFdIZiiFr3t+aNQv15a4H4p5pU3PsFq2Vg=";
+          };
+          chv-ovmf = pkgs.OVMF-cloud-hypervisor.fd;
+        in
+        {
+          "10-chv" = {
+            "/var/lib/nova/hypervisor-fw" = {
+              "L+" = {
+                argument = "${chv-firmware}";
+              };
+            };
+            "/usr/share/cloud-hypervisor/CLOUDHV_EFI.fd" = {
+              "L+" = {
+                argument = "${chv-ovmf}/FV/CLOUDHV.fd";
+              };
+            };
+            "/var/log/libvirt/ch" = {
+              "D" = {
+                user = "root";
+                group = "root";
+                mode = "0755";
+              };
+            };
+          };
+        };
+
+      nova.novaPackage = novaPkg;
+      nova.extraPkgs = [ pkgs.cloud-hypervisor ];
+
+      nova.config = pkgs.writeText "nova.conf" ''
+        [DEFAULT]
+        log_dir = /var/log/nova
+        lock_path = /var/lock/nova
+        state_path = /var/lib/nova
+        rootwrap_config = ${rootwrapConf}
+        compute_driver = libvirt.LibvirtDriver
+        my_ip = 10.0.0.39
+        transport_url = rabbit://openstack:openstack@controller
+
+        [api]
+        auth_strategy = keystone
+
+        [api_database]
+        connection = sqlite:////var/lib/nova/nova_api.sqlite
+
+        [database]
+        connection = sqlite:////var/lib/nova/nova.sqlite
+
+        [glance]
+        api_servers = http://controller:9292
+
+        [keystone_authtoken]
+        www_authenticate_uri = http://controller:5000/
+        auth_url = http://controller:5000/
+        memcached_servers = controller:11211
+        auth_type = password
+        project_domain_name = Default
+        user_domain_name = Default
+        project_name = service
+        username = nova
+        password = nova
+
+        [libvirt]
+        virt_type = ch
+        images_type = raw
+
+        [neutron]
+        auth_url = http://controller:5000
+        auth_type = password
+        project_domain_name = Default
+        user_domain_name = Default
+        region_name = RegionOne
+        project_name = service
+        username = neutron
+        password = neutron
+
+        [os_vif_ovs]
+        ovsdb_connection = unix:/run/openvswitch/db.sock
+
+        [oslo_concurrency]
+        lock_path = /var/lib/nova/tmp
+
+        [placement]
+        region_name = RegionOne
+        project_domain_name = Default
+        project_name = service
+        auth_type = password
+        user_domain_name = Default
+        auth_url = http://controller:5000/v3
+        username = placement
+        password = placement
+
+        [service_user]
+        send_service_user_token = true
+        auth_url = http://controller:5000/
+        auth_strategy = keystone
+        auth_type = password
+        project_domain_name = Default
+        project_name = service
+        user_domain_name = Default
+        username = nova
+        password = nova
+
+        [vnc]
+        enabled = true
+        server_listen = 0.0.0.0
+        server_proxyclient_address = $my_ip
+        novncproxy_base_url = http://controller:6080/vnc_lite.html
+
+        [cells]
+        enable = False
+
+        [os_region_name]
+        openstack =
+      '';
+    };
+
+  testScript =
+    { ... }:
+    ''
+      import json
+      import time
+
+      def retry_until_succeed(machine, cmd, retries = 10):
+        for i in range(retries):
+          print(f"Retrying command until success '{cmd}'. {i + 1}/{retries} retries")
+          time.sleep(1)
+          status, _ = machine.execute(cmd)
+          if status == 0:
+            return True
+        return False
+
+      def print_logfile(machine, filepath):
+        _, out = controllerVM.execute(f"cat {filepath}")
+        print(f"Printing log of: {filepath}")
+        print(out)
+
+      def wait_for_openstack():
+        for i in range(120):
+          print(f"Waiting for openstack network agents and compute nodes to be present ... {i + 1}/120 sec")
+          time.sleep(1)
+          status, out = controllerVM.execute("openstack network agent list -f json")
+          if status != 0:
+            continue
+          net_agents = json.loads(out)
+          status, out = controllerVM.execute("openstack compute service list --service nova-compute -f json")
+          if status != 0:
+            continue
+          compute_nodes = json.loads(out)
+          if len(net_agents) == 4 and len(compute_nodes) == 1:
+            return True
+        return False
+
+      def wait_for_openstack_vm():
+        for i in range(30):
+          print(f"Waiting for openstack server to be active ... {i + 1}/30 sec")
+          time.sleep(1)
+          status, out = controllerVM.execute("openstack server list -f json")
+          if status != 0:
+            continue
+          vms = json.loads(out)
+          if len(vms) == 1 and vms[0]["Status"] == "ACTIVE":
+            return True
+          elif len(vms) == 1 and vms[0]["Status"] == "ERROR":
+            print(out)
+            print_logfile(controllerVM, "/var/log/nova/.nova-manage-wrapped.log")
+            print_logfile(controllerVM, "/var/log/nova/.nova-scheduler-wrapped.log")
+            print_logfile(controllerVM, "/var/log/nova/.nova-api-wrapped.log")
+            print_logfile(controllerVM, "/var/log/nova/.nova-conductor-wrapped.log")
+            print_logfile(controllerVM, "/var/log/neutron/.neutron-server-wrapped.log")
+            print_logfile(controllerVM, "/var/log/neutron/.neutron-openvswitch-agent-wrapped.log")
+            print_logfile(controllerVM, "/var/log/neutron/.neutron-dhcp-agent-wrapped.log")
+            return False
+        return False
+
+      def wait_for_network_namespace():
+        for i in range(30):
+          print(f"Waiting for network namespace to appear ... {i +1}/30 sec")
+          time.sleep(1)
+          net_ns = controllerVM.succeed("ip netns list | awk '{ print $1 }'").strip()
+          if net_ns != "":
+            return net_ns
+        return ""
+
+
+      start_all()
+      controllerVM.wait_for_unit("glance-api.service")
+      controllerVM.wait_for_unit("placement-api.service")
+      controllerVM.wait_for_unit("neutron-server.service")
+      controllerVM.wait_for_unit("nova-scheduler.service")
+      controllerVM.wait_for_unit("nova-conductor.service")
+
+      assert wait_for_openstack()
+
+      controllerVM.succeed("systemctl start nova-host-discovery.service")
+      controllerVM.wait_for_unit("nova-host-discovery.service")
+      controllerVM.succeed("systemctl start openstack-create-vm.service")
+      controllerVM.wait_for_unit("openstack-create-vm.service")
+      assert wait_for_openstack_vm()
+
+      # Check that our Cloud Hypervisor driver is loaded and correctly reported
+      assert retry_until_succeed(controllerVM, "openstack hypervisor list")
+      hypervisor_list = json.loads(controllerVM.succeed("openstack hypervisor list -f json"))
+      assert hypervisor_list[0]["Hypervisor Type"] == "CH"
+
+      computeVM.succeed("pgrep -f cloud-hypervisor")
+
+      vm_state = json.loads(controllerVM.succeed("openstack server show test_vm -f json"))
+
+      vm_ip = vm_state["addresses"]["provider"][0]
+      assert vm_ip.startswith("192.168.44")
+
+      net_ns = wait_for_network_namespace()
+      assert net_ns != ""
+
+      # Ping the OpenStack VM from the controller host. We use the network
+      # namespace dedicated for the VM to ping it.
+      assert retry_until_succeed(controllerVM, f"ip netns exec {net_ns} ping -c 1 {vm_ip}", 30)
+
+      # Test that deletion and cleanup works as expected
+      controllerVM.succeed("openstack server delete test_vm")
+
+      assert retry_until_succeed(controllerVM, "openstack server list")
+      server_list = json.loads(controllerVM.succeed("openstack server list -f json"))
+      assert len(server_list) == 0
+
+      computeVM.fail("pgrep -f cloud-hypervisor")
+    '';
+}
