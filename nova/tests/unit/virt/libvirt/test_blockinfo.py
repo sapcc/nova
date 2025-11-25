b4a39c49c380604dc876053a5a3dac9b708723a6
Fabian Wiesel <fabian.wiesel@sap.com>
2025-11-25 12:32:20 +0100

    libvirt: Provide default disk-bus for cloudhypervisor
    It is fairly straight-forward with CloudHypervisor, it only supports
    virtio (or vhost). So, we return that. We leave the cdrom bus blank,
    as there is no bus we support that would be needed for cdroms.
    We cannot raise an error, as `blockinfo.get_disk_info()` is called
    by `spawn()`, and it queries also the cdrom-bus, even if it isn't
    needed.
    
    Change-Id: I5911d9e82da86e8d6f1e2cf92b07e13934878700

--- nova/tests/unit/virt/libvirt/test_blockinfo.py
+++ nova/tests/unit/virt/libvirt/test_blockinfo.py
@@ -1060,8 +1060,10 @@ class LibvirtBlockInfoTest(test.NoDBTestCase):
                 ('parallels', 'scsi', None, 'disk', 'scsi'),
                 ('parallels', None, None, 'disk', 'scsi'),
                 ('parallels', None, 'ide', 'cdrom', 'ide'),
-                ('parallels', None, None, 'cdrom', 'ide')
-                )
+                ('parallels', None, None, 'cdrom', 'ide'),
+                ('ch', None, None, 'disk', 'virtio'),
+                ('ch', None, None, 'cdrom', None),
+        )
         for hv, dbus, cbus, dev, res in expected:
             props = {}
             if dbus is not None:
