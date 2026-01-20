7bf3c303b31bea0cd7c3b7bbafbadad62c38db32
Paul Kroeher <paul.kroeher@cyberus-technology.de>
2026-01-20 09:53:11 +0100

    docs: update documentation
    On-behalf-of: SAP paul.kroeher@sap.com

--- nix/tests/default.nix
+++ nix/tests/default.nix
@@ -17,6 +17,11 @@ let
     chv-numa = pkgs.callPackage ./chv-numa.nix {
       inherit nixosModules novaPkg generateRootwrapConf chvModule;
     };
+
+    # Test implicit BDF assignment.
+    # Test cases:
+    #   Reboot VM via openstack and check BDF assignments
+    #   VM Live migration and check BDF assignments
     chv-bdf-live-migration = pkgs.callPackage ./chv-bdf-live-migration.nix {
       inherit nixosModules novaPkg generateRootwrapConf chvModule;
       testScriptFile = ./chv-bdf-live-migration.py;
