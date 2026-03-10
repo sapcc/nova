ec4423156027a1b992216b0052e8fdfd372edbec
Stefan Kober <stefan.kober@cyberus-technology.de>
2026-03-10 09:21:36 +0100

    libvirt/ch: use ch:///system as default URI
    Change-Id: Id627b771cd9cbda8e136e09ae11aef74810469fc
    On-behalf-of: SAP stefan.kober@sap.com
    Signed-off-by: Stefan Kober <stefan.kober@cyberus-technology.de>
    (cherry picked from commit 0eb7bc670afe062c8ad2d2345bd1f36e01e97c67)

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -1479,7 +1479,7 @@ class LibvirtDriver(driver.ComputeDriver):
         elif CONF.libvirt.virt_type == 'parallels':
             uri = CONF.libvirt.connection_uri or 'parallels:///system'
         elif CONF.libvirt.virt_type == 'ch':
-            uri = CONF.libvirt.connection_uri or 'ch:///session'
+            uri = CONF.libvirt.connection_uri or 'ch:///system'
         else:
             uri = CONF.libvirt.connection_uri or 'qemu:///system'
         return uri
@@ -1490,6 +1490,7 @@ class LibvirtDriver(driver.ComputeDriver):
             'kvm': 'qemu+%(scheme)s://%(dest)s/system',
             'qemu': 'qemu+%(scheme)s://%(dest)s/system',
             'parallels': 'parallels+tcp://%(dest)s/system',
+            'ch': 'ch+%(scheme)s://%(dest)s/system',
         }
         dest = oslo_netutils.escape_ipv6(dest)
 
