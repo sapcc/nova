sapcc/nova

commit 88b00b69da0dddef45ac29e097938ee9a910fb26

libvirt: call recover method from _live_migration
Previously, `_live_migration` did not call `recover_method` when
exceptions occurred.

To improve cleanup, we add `recover_method` invocation for exceptions
during the setup phase before or during the spawning of the
`_live_migration_operation` thread.

We also invoke `recover_method` if a later exception occurs and libvirt
successfully aborts the migration job.

Change-Id: I46822b7642c71cc12b7ab94e36c19c3b53d48c4c

nova/virt/libvirt/driver.py
@@ -10774,15 +10774,20 @@ class LibvirtDriver(driver.ComputeDriver):
         operation, and then this thread monitors the progress of
         migration and controls its operation
         """
+        try:
+            guest = self._host.get_guest(instance)
 
-        guest = self._host.get_guest(instance)
-
-        disk_paths = []
-        device_names = []
-        if (migrate_data.block_migration and
-                CONF.libvirt.virt_type != "parallels"):
-            disk_paths, device_names = self._live_migration_copy_disk_paths(
-                context, instance, guest)
+            disk_paths = []
+            device_names = []
+            if (migrate_data.block_migration and
+                    CONF.libvirt.virt_type != "parallels"):
+                disk_paths, device_names = \
+                    self._live_migration_copy_disk_paths(context, instance,
+                                                         guest)
+        except Exception:
+            with excutils.save_and_reraise_exception():
+                LOG.exception("Failed live migration setup", instance=instance)
+                recover_method(context, instance, dest, migrate_data)
 
         opthread = utils.spawn(self._live_migration_operation,
                                      context, instance, dest,
@@ -10790,44 +10795,48 @@ class LibvirtDriver(driver.ComputeDriver):
                                      migrate_data, guest,
                                      device_names)
 
-        finish_event = eventlet.event.Event()
-        self.active_migrations[instance.uuid] = deque()
-
-        def thread_finished(thread, event):
-            LOG.debug("Migration operation thread notification",
-                      instance=instance)
-            event.send()
-        opthread.link(thread_finished, finish_event)
-
-        # Let eventlet schedule the new thread right away
-        time.sleep(0)
-
         try:
+            finish_event = eventlet.event.Event()
+            self.active_migrations[instance.uuid] = deque()
+
+            def thread_finished(thread, event):
+                LOG.debug("Migration operation thread notification",
+                          instance=instance)
+                event.send()
+            opthread.link(thread_finished, finish_event)
+
+            # Let eventlet schedule the new thread right away
+            time.sleep(0)
+
             LOG.debug("Starting monitoring of live migration",
                       instance=instance)
             self._live_migration_monitor(context, instance, guest, dest,
                                          post_method, recover_method,
                                          block_migration, migrate_data,
                                          finish_event, disk_paths)
-        except Exception as ex:
-            LOG.warning("Error monitoring migration: %(ex)s",
-                        {"ex": ex}, instance=instance, exc_info=True)
-            # NOTE(aarents): Ensure job is aborted if still running before
-            # raising the exception so this would avoid the migration to be
-            # done and the libvirt guest to be resumed on the target while
-            # the instance record would still related to the source host.
-            try:
-                # If migration is running in post-copy mode and guest
-                # already running on dest host, libvirt will refuse to
-                # cancel migration job.
-                self.live_migration_abort(instance)
-            except libvirt.libvirtError:
-                LOG.warning("Error occured when trying to abort live ",
-                            "migration job, ignoring it.", instance=instance)
-            raise
-        finally:
             LOG.debug("Live migration monitoring is all done",
                       instance=instance)
+        except Exception:
+            with excutils.save_and_reraise_exception():
+                LOG.exception("Error during live migration monitoring",
+                              instance=instance)
+                # NOTE(aarents): Ensure job is aborted if still running before
+                # raising the exception so this would avoid the migration to be
+                # done and the libvirt guest to be resumed on the target while
+                # the instance record would still related to the source host.
+                try:
+                    # If migration is running in post-copy mode and guest
+                    # already running on dest host, libvirt will refuse to
+                    # cancel migration job.
+                    self.live_migration_abort(instance)
+                except libvirt.libvirtError:
+                    LOG.warning("Error occured when trying to abort live ",
+                                "migration job, ignoring it.",
+                                instance=instance)
+                else:
+                    # If the migration job was successfully aborted, call the
+                    # recover_method for cleanup.
+                    recover_method(context, instance, dest, migrate_data)
 
     def _is_post_copy_enabled(self, migration_flags):
         return (migration_flags & libvirt.VIR_MIGRATE_POSTCOPY) != 0

nova/tests/unit/virt/libvirt/test_driver.py
@@ -14410,7 +14410,11 @@ class LibvirtConnTestCase(test.NoDBTestCase,
     def _test_live_migration_main(self, mock_abort, mock_copy_disk_path,
                                   mock_running, mock_guest, mock_monitor,
                                   mock_thread, mock_conn,
-                                  mon_side_effect=None):
+                                  mon_side_effect=None,
+                                  abort_side_effect=None,
+                                  guest_side_effect=None,
+                                  copy_disk_path_side_effect=None,
+                                  thread_side_effect=None):
         drvr = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), False)
         instance = objects.Instance(**self.test_instance)
 
@@ -14420,47 +14424,103 @@ class LibvirtConnTestCase(test.NoDBTestCase,
         migrate_data = objects.LibvirtLiveMigrateData(block_migration=True)
         disks_to_copy = (['/some/path/one', '/test/path/two'],
                          ['vda', 'vdb'])
-        mock_copy_disk_path.return_value = disks_to_copy
 
+        mock_guest.side_effect = guest_side_effect
         mock_guest.return_value = guest
+        mock_copy_disk_path.side_effect = copy_disk_path_side_effect
+        mock_copy_disk_path.return_value = disks_to_copy
+        mock_thread.side_effect = thread_side_effect
         mock_monitor.side_effect = mon_side_effect
+        mock_abort.side_effect = abort_side_effect
 
         def fake_post():
             pass
 
-        def fake_recover():
-            pass
+        recover_method = mock.MagicMock()
+        dest = "fakehost"
 
-        if mon_side_effect:
-            self.assertRaises(mon_side_effect, drvr._live_migration,
-                              self.context, instance, "fakehost", fake_post,
-                              fake_recover, True, migrate_data)
-            mock_abort.assert_called_once_with(instance)
+        # Determine which exception we expect based on what fails first
+        setup_side_effect = (guest_side_effect or
+                             copy_disk_path_side_effect)
+        side_effect = (setup_side_effect or thread_side_effect or
+                       mon_side_effect)
+        if side_effect:
+            self.assertRaises(side_effect, drvr._live_migration, self.context,
+                              instance, dest, fake_post, recover_method, True,
+                              migrate_data)
         else:
-            drvr._live_migration(self.context, instance, "fakehost", fake_post,
-                                 fake_recover, True, migrate_data)
+            drvr._live_migration(self.context, instance, dest, fake_post,
+                                 recover_method, True, migrate_data)
 
+        if setup_side_effect:
+            # Setup failure
+            recover_method.assert_called_once_with(
+                self.context, instance, dest, migrate_data)
+            return
+
+        mock_guest.assert_called_once_with(instance)
         mock_copy_disk_path.assert_called_once_with(self.context, instance,
                                                     guest)
+        mock_thread.assert_called_once_with(
+            drvr._live_migration_operation,
+            self.context, instance, dest, True,
+            migrate_data, guest, disks_to_copy[1])
+
+        if thread_side_effect:
+            # Thread spawn failure
+            recover_method.assert_not_called()
+            return
 
         class AnyEventletEvent(object):
             def __eq__(self, other):
                 return type(other) == eventlet.event.Event
 
-        mock_thread.assert_called_once_with(
-            drvr._live_migration_operation,
-            self.context, instance, "fakehost", True,
-            migrate_data, guest, disks_to_copy[1])
         mock_monitor.assert_called_once_with(
-            self.context, instance, guest, "fakehost",
-            fake_post, fake_recover, True,
+            self.context, instance, guest, dest,
+            fake_post, recover_method, True,
             migrate_data, AnyEventletEvent(), disks_to_copy[0])
+
+        if mon_side_effect:
+            # Monitoring phase failure
+            mock_abort.assert_called_once_with(instance)
+            # recover_method is only called if abort succeeds
+            if abort_side_effect:
+                recover_method.assert_not_called()
+            else:
+                recover_method.assert_called_once_with(
+                    self.context, instance, dest, migrate_data)
+            return
+
+        recover_method.assert_not_called()
 
     def test_live_migration_main(self):
         self._test_live_migration_main()
 
-    def test_live_migration_main_monitoring_failed(self):
+    def test_live_migration_setup_get_guest_fails(self):
+        # When get_guest fails during setup, recover_method is called
+        self._test_live_migration_main(
+            guest_side_effect=Exception)
+
+    def test_live_migration_setup_copy_disk_paths_fails(self):
+        # When _live_migration_copy_disk_paths fails during setup,
+        # recover_method is called
+        self._test_live_migration_main(
+            copy_disk_path_side_effect=Exception)
+
+    def test_live_migration_spawn_fails(self):
+        # When utils.spawn fails, recover_method should NOT be called
+        self._test_live_migration_main(
+            thread_side_effect=Exception)
+
+    def test_live_migration_monitoring_fails(self):
         self._test_live_migration_main(mon_side_effect=Exception)
+
+    def test_live_migration_monitoring_fails_abort_fails(self):
+        # When monitoring fails and abort also fails (e.g., post-copy mode),
+        # recover_method should NOT be called
+        self._test_live_migration_main(
+            mon_side_effect=Exception,
+            abort_side_effect=fakelibvirt.libvirtError("abort failed"))
 
     @mock.patch.object(host.Host, "get_connection", new=mock.Mock())
     @mock.patch.object(utils, "spawn", new=mock.Mock())
@@ -14487,14 +14547,14 @@ class LibvirtConnTestCase(test.NoDBTestCase,
         instance = objects.Instance(**self.test_instance)
         post_method = mock.Mock()
         migrate_data = mock.Mock()
+        recover_method = mock.Mock()
         disks_to_copy = (['/some/path/one', '/test/path/two'],
                          ['vda', 'vdb'])
         mock_copy_disk_paths.return_value = disks_to_copy
 
         func = drvr._live_migration
         args = (self.context, instance, mock.sentinel.dest, post_method,
-                mock.sentinel.recover_method, mock.sentinel.block_migration,
-                migrate_data)
+                recover_method, mock.sentinel.block_migration, migrate_data)
 
         if expect_success:
             func(*args)

commit 7433346a3fa283461ce293781323c56cbac53713

libvirt: fix _live_migration logging
Fix a logging bug in the LibVirt driver's `_live_migration` method that
was introduced by commit `88b00b69da0dddef45ac29e097938ee9a910fb26`.

Also fix a typo in the log message.

Change-Id: Iaaba719491e4eb24dd18bdf2502e33deea4e7a1d

nova/virt/libvirt/driver.py
@@ -10836,7 +10836,7 @@ class LibvirtDriver(driver.ComputeDriver):
                     # cancel migration job.
                     self.live_migration_abort(instance)
                 except libvirt.libvirtError:
-                    LOG.warning("Error occured when trying to abort live ",
+                    LOG.warning("Error occurred when trying to abort live "
                                 "migration job, ignoring it.",
                                 instance=instance)
                 else: