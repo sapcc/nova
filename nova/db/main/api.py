8e594a214197ffab190f5c131bafe993399f51b2
Julien Le Jeune <julien.le-jeune@ovhcloud.com>
2026-04-20 11:09:47 +0200

    Cherry-Pick: Reset the mapped field of nodes at service deletion
    Make sure that the 'mapped' field of compute nodes is reset
    to 0 at nova-compute service deletion, using a single efficient
    UPDATE statement.
    
    This fixes the bug where deleting a nova-compute service would
    leave the 'mapped' field set to 1, preventing nova-manage cell_v2
    discover_hosts from recreating the missing host mapping.
    
    The fix combines the soft-delete operation with resetting the
    mapped field in a single atomic UPDATE statement, avoiding the
    need for separate queries.
    
    Closes-Bug: #2085135
    Change-Id: I774d4312d7e94af94d960f7269717a00fa08c583
    Signed-off-by: Julien Le Jeune <julien.le-jeune@ovhcloud.com>

--- nova/db/main/api.py
+++ nova/db/main/api.py
@@ -383,18 +383,18 @@ def service_destroy(context, service_id):
     if service.binary == 'nova-compute':
         # TODO(sbauza): Remove the service_id filter in a later release
         # once we are sure that all compute nodes report the host field
-        query = model_query(context, models.ComputeNode).\
-            filter(sql.or_(
-            models.ComputeNode.service_id == service_id,
-            models.ComputeNode.host == service['host']))
-
-        # NOTE(jlejeune): Make sure that the mapped field is set to 0
+        # NOTE(jlejeune): Make sure that the mapped field of the
+        # relevant compute nodes is set to 0
         # See https://bugs.launchpad.net/nova/+bug/2085135.
-        compute_ref = query.first()
-        if compute_ref:
-            compute_ref.update({'mapped': 0})
-
-        query.soft_delete(synchronize_session=False)
+        model_query(context, models.ComputeNode).\
+            filter(sql.or_(
+                models.ComputeNode.service_id == service_id,
+                models.ComputeNode.host == service['host'])).\
+            update({'deleted': models.ComputeNode.id,
+                    'updated_at': models.ComputeNode.updated_at,
+                    'deleted_at': timeutils.utcnow(),
+                    'mapped': 0},
+                   synchronize_session=False)
 
 
 @pick_context_manager_reader
